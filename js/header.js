ed4edf89bd09569d9fb400bb3236df23ca4965e9
dpgg25 <dpgg12@naver.com>
2021-06-17 15:33:18 +0900

    headerjs

--- js/header.js
+++ js/header.js
@@ -6,12 +6,10 @@ const gnbMenu = document.querySelector('.m_layout .gnb_wrap');
 const gnbfirstUl = document.querySelector('.m_layout .gnb_wrap .gnb_menu > ul');
 const gnbA = document.querySelector('.m_layout .gnb_wrap .gnb_menu > ul > li > a');
 const gnbsecondUl = document.querySelector('.m_layout .gnb_wrap .gnb_menu > ul > li > ul');
-// const header = document.querySelector(".hd_wrap");
-// const headerHeight = header.getBoundingClientRect().height;
+const headerbar = document.querySelector(".m_layout .hd_wrap");
 openhamBtn.addEventListener('click', openHam, false);
 closehamBtn.addEventListener('click', closeHam, false);
 gnbfirstUl.addEventListener('click', openGnb, false);
-// window.addEventListener('scroll', scrollMenu, false);
 
 function openHam() {
   body.classList.add('menu_scroll_hidden');
@@ -25,13 +23,15 @@ function closeHam() {
   gnbMenu.classList.remove('on');
 }
 
-// function scrollMenu() {
-//   if (window.scrollY > headerHeight) {
-//     header.classList.add('scroll');
-//   } else {  
-//     header.classList.remove('scroll');
-//   }
-// }; 왜 안 돼
+function headerbarToggle() {
+  var topScroll = headerbar.offsetTop;
+  var currentScroll = window.pageYOffset;
+  if (topScroll < currentScroll) {
+    headerbar.classList.add('scroll');
+  } else {  
+    headerbar.classList.remove('scroll');
+  }
+};
 
 function openGnb(e) {
   e.preventDefault();
